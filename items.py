c06c296733f3465ea6a368ed31a2638fbd364c4b
codewithpatch <chesterdealday@gmail.com>
2020-08-01 00:08:28 +0200

    Data model

---
+++ items.py
@@ -0,0 +1,16 @@
+class FreelancerScraperItem:
+    title = str()
+    hourly_rate = int()
+    description = str()
+    location = str()
+    education = str()
+    total_earnings = float()
+    total_jobs = int()
+    skills_and_expertise = list() # a list of tags
+    work_history = dict()
+    # work title
+    # Job description
+    # experience level
+    # Skills required
+    # Client location
+    # date
