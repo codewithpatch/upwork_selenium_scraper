71df9cd17597b63991b416006155dc1e8f9de62b
codewithpatch <chesterdealday@gmail.com>
2020-08-02 11:29:34 +0200

    Cleanup comments

--- items.py
+++ items.py
@@ -6,22 +6,13 @@ class FreelancerScraperItem:
     education = list()
     total_earnings = float()
     total_jobs = int()
-    skills_and_expertise = list() # a list of tags
+    skills_and_expertise = list()
     work_history = dict()
-    # work title
-    # Job description
-    # experience level
-    # Skills required
-    # Client location
-    # date
+
 
 class ProjectScraperItem:
     title = str()
     project_rate = int()
     description = str()
-    location = str() # location is location of the employer?
-    # education = list()
-    # total_earnings = float()
-    # total_jobs = int()
-    skills_and_expertise = list()  # a list of tags
-    # work_history = dict()
+    location = str()
+    skills_and_expertise = list()
